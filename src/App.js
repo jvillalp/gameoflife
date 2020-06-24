7fb2961009919264b10af749152d734b85251f3f
Juana Villalpando <juana.v14@gmail.com>
2020-06-24 12:04:59 -0700

    create grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const numRows = 50;
+const numCols = 50;
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows
+  });
+
+  console.log(grid)
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World!
+      <header className="App-header" style={{
+        display:'grid',
+        gridTemplateColumns:`repeat(${numCols}, 20px)`
+    }}>
+        {grid.map((rows, i) =>
+          rows.map((col, k) => 
+          <div 
+            key = {`${i}-${k}`}
+            onClick={()=> {
+              grid[i][k] = 1
+            }}
+            style={{ width: 20, 
+            height: 20, backgroundColor:[i][k] ? 'pink' : undefined,
+            border: 'solid 1px white'}} 
+            />) )}
       </header>
     </div>
   );
